f646aaffc5d090f74b8cd66fd8df27c66af6c005
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-07-01 16:29:27 -0500

    prepare funcs

--- prepare.py
+++ prepare.py
@@ -115,3 +115,44 @@ def scale_data(train, validate, test):
     test_scaled.columns = test.columns
 
     return train_scaled, validate_scaled, test_scaled
+
+    #######################################################################################################
+
+def percentage_stacked_plot(columns_to_plot, title, df):
+    
+    '''
+    Returns a 100% stacked plot of the response variable for independent variable of the list columns_to_plot.
+    Parameters: columns_to_plot (list of string): Names of the variables to plot
+    '''
+    
+    number_of_columns = 2
+    number_of_rows = math.ceil(len(columns_to_plot)/2)
+
+    # create a figure
+    fig = plt.figure(figsize=(12, 5 * number_of_rows)) 
+    fig.suptitle(title, fontsize=22,  y=.95)
+ 
+
+    # loop to each column name to create a subplot
+    for index, column in enumerate(columns_to_plot, 1):
+
+        # create the subplot
+        ax = fig.add_subplot(number_of_rows, number_of_columns, index)
+
+        # calculate the percentage of observations of the response variable for each group of the independent variable
+        # 100% stacked bar plot
+        prop_by_independent = pd.crosstab(df[column], df['defense_budget']).apply(lambda x: x/x.sum()*100, axis=1)
+
+        prop_by_independent.plot(kind='bar', ax=ax, stacked=True,
+                                 rot=0, color=['#94bad4','#ebb086'])
+
+        # set the legend in the upper right corner
+        ax.legend(loc="upper right", bbox_to_anchor=(0.62, 0.5, 0.5, 0.5),
+                  title='Churn', fancybox=True)
+
+        # eliminate the frame from the plot
+        spine_names = ('top', 'right', 'bottom', 'left')
+        for spine_name in spine_names:
+            ax.spines[spine_name].set_visible(False)
+
+    return percentage_stacked_plot
